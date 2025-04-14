fde0a9b28e309a56b71e75f6441d619157a2a5dc
Daisy Matlih <dmatlih@nemsu.edu.ph>
2025-04-14 18:50:32 +0800

    trial

--- resources/views/user/pages/python_submission.blade.php
+++ resources/views/user/pages/python_submission.blade.php
@@ -228,7 +228,7 @@
                     Swal.fire({
                         icon: "error",
                         title: "Submission Failed!",
-                        text: "There was an error submitting your code. Please try again." + xhr.responseJSON.errors,
+                        text: "There was an error submitting your code. Please try again." + xhr.responseText,
                         confirmButtonColor: "#d33"
                     });
                 }
