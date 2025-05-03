75b3c2fed9dd25c6b5afb2265aadb0fb316582b2
Daisy Matlih <dmatlih@nemsu.edu.ph>
2025-05-03 10:02:31 +0800

    Seed

--- database/seeders/InstructorsSeeder.php
+++ database/seeders/InstructorsSeeder.php
@@ -12,9 +12,106 @@ class InstructorsSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'email' => 'jmcpalen@nemsu.edu.ph',
+                'email' => 'rcgbandoy@nemsu.edu.ph',
+                'name' => 'Radz Chirro G. Bandoy',
                 'status' => 'active',
-                'account_type' => 'instructor'
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'jlbarte@nemsu.edu.ph',
+                'name' => 'Jericho L. Barte',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'adbolanos@nemsu.edu.ph',
+                'name' => 'Ashlee Joshua Bolaños',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'rnburlasa@nemsu.edu.ph',
+                'name' => 'Romel N. Burlasa',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'gdbutron@nemsu.edu.ph',
+                'name' => 'Glyzle D. Butron',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'jamcadao@nemsu.edu.ph',
+                'name' => 'Junard Adrian M. Cadao',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'racananes@nemsu.edu.ph',
+                'name' => 'Ronnel Cañanes',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'jjdaguiso@nemsu.edu.ph',
+                'name' => 'Jaybee J. Daguiso',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'fpdeleguerjr@nemsu.edu.ph',
+                'name' => 'Fernando Deleguer Jr',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'mrjcdivino@nemsu.edu.ph',
+                'name' => 'Ma. Rhea Jane C. Divino',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'mdetchon@nemsu.edu.ph',
+                'name' => 'Meljoy D. Etchon',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'erafestejo@nemsu.edu.ph',
+                'name' => 'Earl Rey A. Festejo',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'ljsgallardo@nemsu.edu.ph',
+                'name' => 'Louie Jay Gallardo',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'naogardigo@nemsu.edu.ph',
+                'name' => 'Nick Ale Gardigo',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'spmaunes@nemsu.edu.ph',
+                'name' => 'Sandara P. Maunes',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'rquijada@nemsu.edu.ph',
+                'name' => 'Rommel Quijada',
+                'status' => 'active',
+                'account_type' => 'student'
+            ],
+            [
+                'email' => 'kjcrapal@nemsu.edu.ph',
+                'name' => 'Khezar Jhon C. Rapal',
+                'status' => 'active',
+                'account_type' => 'student'
             ],
 
         ]);
